f9f33c4eb6e386db2b76c1a76c80edca84a9eb63
penrose15 <seongju99@naver.com>
2023-01-03 00:21:12 +0900

    chore : add Pageable at TodoRepository

--- src/main/java/com/example/to_do_list/repository/TodoRepository.java
+++ src/main/java/com/example/to_do_list/repository/TodoRepository.java
@@ -2,6 +2,7 @@ package com.example.to_do_list.repository;
 
 import com.example.to_do_list.domain.Todo;
 import com.example.to_do_list.dto.todo.TodoResponsesDto;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,5 +12,5 @@ import java.time.LocalDate;
 public interface TodoRepository extends JpaRepository<Todo, Long> {
     @Query("select new com.example.to_do_list.dto.todo.TodoResponsesDto(t.title, t.done) " +
             " from Todo t where :date >= t.date and :date <= t.endDate")
-    Slice<TodoResponsesDto> findByDateNow(LocalDate date);
+    Slice<TodoResponsesDto> findByDateNow(Pageable pageable, LocalDate date);
 }
