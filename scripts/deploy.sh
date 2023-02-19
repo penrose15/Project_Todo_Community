5449ceee78f15f3d5ba1e321150881679e208858
penrose15 <seongju99@naver.com>
2023-02-19 17:08:05 +0900

    test : add deploy.sh, switch.sh, test.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,13 +5,16 @@ sudo docker ps -a -q --filter "name=hsj" | grep -q . && docker stop hsj && docke
 sudo docker ps -a -q --filter "name=myredis" | grep -q . && docker stop myredis && docker rm myredis | true
 
 # 기존 이미지 삭제
-sudo docker rmi admin1125/hsj:1.0
+sudo docker rmi admin1125/hsj:latest
 
 # 도커허브 이미지 pull
-sudo docker pull admin1125/hsj:1.0
+#sudo docker pull admin1125/hsj:1.0
+
+#여기에 nginx 실행하고 포트 나누면 될 듯하다
+sudo sh /home/ec2-user/test.sh
 
 # 도커 run
-docker run -d -p 8080:8080 --name hsj admin1125/hsj:1.0
+#docker run -d -p 8080:8080 --name hsj admin1125/hsj:1.0
 # redis start
 docker run -d --name myredis -p 6379:6379 redis
 
---
+++ src/main/java/com/example/to_do_list/controller/ProfileController.java
@@ -0,0 +1,26 @@
+package com.example.to_do_list.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@RestController
+@RequiredArgsConstructor
+public class ProfileController {
+
+    private final Environment env;
+
+    @GetMapping("/profile")
+    public String getProfile() {
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
+    }
+
+}
