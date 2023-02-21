290e1eefba5cab1ccd683283fd8a931ec0304016
penrose15 <seongju99@naver.com>
2023-02-22 01:05:06 +0900

    fix : deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 sudo docker ps -a -q --filter "name=hsj" | grep -q . && docker stop hsj && docker rm hsj | true
 
 sudo docker ps -a -q --filter "name=myredis" | grep -q . && docker stop myredis && docker rm myredis | true
@@ -16,9 +14,9 @@ echo "> hello"
 #sudo sh /home/ec2-user/test.sh
 
 # 도커 run
-docker run -d -p 8080:8080 --name hsj admin1125/hsj:1.0
+docker run -d -p 8080:8080 -v /home/ec2-user:/config --name hsj admin1125/hsj:1.0
 # redis start
 docker run -d --name myredis -p 6379:6379 redis
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
-docker rmi -f $(docker images -f "dangling=true" -q) || true
+#docker rmi -f $(docker images -f "dangling=true" -q) || true
