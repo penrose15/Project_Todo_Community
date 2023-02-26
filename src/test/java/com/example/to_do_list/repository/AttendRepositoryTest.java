0c34b310e3b2a14c24cf851439a01f77c8e88d61
penrose15 <seongju99@naver.com>
2023-02-27 01:06:01 +0900

    fix : test 수정 (active profile 추가)

--- src/test/java/com/example/to_do_list/repository/AttendRepositoryTest.java
+++ src/test/java/com/example/to_do_list/repository/AttendRepositoryTest.java
@@ -14,7 +14,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @DataJpaTest
 @Import({JPAConfig.class, QueryDslConfig.class})
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-//@ActiveProfiles("set1")
+@ActiveProfiles("prod1")
 public class AttendRepositoryTest {
 
     @Autowired
