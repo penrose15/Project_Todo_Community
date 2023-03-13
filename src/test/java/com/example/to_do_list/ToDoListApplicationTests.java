ea41050a3fe83df5070108117dcda99a0dd34283
penrose15 <seongju99@naver.com>
2023-03-13 23:43:06 +0900

    feat : test DB 분리

--- src/test/java/com/example/to_do_list/ToDoListApplicationTests.java
+++ src/test/java/com/example/to_do_list/ToDoListApplicationTests.java
@@ -1,10 +1,13 @@
 package com.example.to_do_list;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@SpringBootTest(properties = "classpath:application-test.yml")
+@ExtendWith(SpringExtension.class)
 //@ActiveProfiles("prod1")
 class ToDoListApplicationTests {
 
